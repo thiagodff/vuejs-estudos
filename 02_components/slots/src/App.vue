5faa0fc3ddf0a06f8dc6704cf06f19cd83050fbd
thiagodff <thiago.fdornelles@gmail.com>
2020-06-14 13:38:54 -0300

    components: slots

---
+++ 02_components/slots/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+
+    <List>
+      <template v-slot:header>
+        <h1>This is a header child</h1>
+      </template>
+
+      <template v-slot:default>
+        <h3>Default slot get others child elements</h3>
+
+        <ul>
+          <li>hi</li>
+        </ul>
+      </template>
+
+      <!-- shorthand -->
+      <template #footer="{ company }">
+        <h3>{{ company.name }} Footer slot child</h3>
+      </template>
+    </List>
+  </div>
+</template>
+
+<script>
+import List from "./components/List.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello VueJs",
+    };
+  },
+  components: {
+    List,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ 02_components/slots/src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <slot name="header"></slot>
+
+    <ul>
+      <li>Home</li>
+      <li>Products</li>
+    </ul>
+    <slot name="default"></slot>
+
+    <slot name="footer" :company="company"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      company: {
+        name: "© Thiago",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
