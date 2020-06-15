80b93cc622fbdee0b65971018c30123e3abb09d6
thiagodff <thiago.fdornelles@gmail.com>
2020-06-15 08:48:03 -0300

    animations: transition group

---
+++ 03_animations/transition_group.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app">
+    <!-- por padrao a tag e span -->
+    <transition-group tag="ul">
+      <li v-for="(product, index) in cart" :key="product.name">
+        {{ product.name }}: {{ product.price }}
+        <button @click="removeProduct(index)">X</button>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cart: [
+        {
+          name: "Notebook",
+          price: 2999
+        },
+        {
+          name: "Smartphone",
+          price: 1999
+        }
+      ]
+    };
+  },
+  methods: {
+    removeProduct(index) {
+      this.cart.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all .3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+</style>
