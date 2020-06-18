6695a8455cf54e0aa84de4579040130304752654
thiagodff <thiago.fdornelles@gmail.com>
2020-06-18 13:25:47 -0300

    ranek: initial commit

---
+++ ranek/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
