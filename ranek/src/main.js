d4a57fe446816e1c268654422c8051f411fe010a
thiagodff <thiago.fdornelles@gmail.com>
2020-06-19 13:42:06 -0300

    ranek: format price filter

--- ranek/src/main.js
+++ ranek/src/main.js
@@ -3,7 +3,23 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Loading from '@/components/Loading';
+
 Vue.config.productionTip = false;
+
+Vue.component('Loading', Loading);
+
+Vue.filter('formatPrice', value => {
+  value = Number(value);
+  if (!isNaN(value)) {
+    return value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  } else {
+    return '';
+  }
+});
 
 new Vue({
   router,
