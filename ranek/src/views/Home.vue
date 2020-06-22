c312699f44969bfd249efe5370758d4045900295
thiagodff <thiago.fdornelles@gmail.com>
2020-06-21 23:59:51 -0300

    ranek: home page responsivity

--- ranek/src/views/Home.vue
+++ ranek/src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="home">
-    <h1>Hello Vue</h1>
-  </div>
+  <section>
+    <h1>👍Compre ou Venda👎</h1>
+
+    <ProductsSearch />
+    <ProductsList />
+  </section>
 </template>
 
 <script>
+import ProductsSearch from '@/components/ProductsSearch';
+import ProductsList from '@/components/ProductsList';
+
 export default {
   name: 'Home',
+  components: {
+    ProductsSearch,
+    ProductsList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  font-size: 2rem;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
