4b9b17b8a54413a1e71aff2c0db2608925ce471d
thiagodff <thiago.fdornelles@gmail.com>
2020-06-22 23:31:10 -0300

    ranek: config vscode root import

---
+++ ranek/vue.config.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//   // quando gerar a build ele vai criar um caminho relativo para seu código vue
+//   // dentro de /vue/js, isso serve para quando vc quer inserir o vue em seu site
+//   // e não fazer ele do zero só com vue
+//   publicPathPath: '/vue',
+// };
